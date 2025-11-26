6a03ef9fa0317be04df5ceabcec40a176248e985
Mathew Sabu <internshipmathewsabu04@gmail.com>
2025-11-26 17:32:12 -0500

    Created the Admin function

--- src/app/lib/useAuth.js
+++ src/app/lib/useAuth.js
@@ -1,16 +1,25 @@
 import { useState, useEffect } from "react";
 import { auth } from "./fireabase";
+import { userIsAdmin } from "./reads";
 
 export const useAuth = () => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [admin, setAdmin] = useState(false);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setUser(user);
+      if (user) {
+        const isAdmin = userIsAdmin(user.uid);
+        setAdmin(isAdmin);
+        if (isAdmin) {
+          window.alert("You are an admin");
+        }
+      }
       setLoading(false);
     });
     return () => unsubscribe();
   }, []);
-  return { user, loading };
+  return { user, loading, admin };
 };
